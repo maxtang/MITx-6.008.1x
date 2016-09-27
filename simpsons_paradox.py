209aa41211b616573752604f799a19b3587e43e8
max <max@localhost.localdomain>
2016-09-28 07:37:46 +0800

    updates the associated graphs on notebook

--- simpsons_paradox.py
+++ simpsons_paradox.py
@@ -6,39 +6,40 @@
 from simpsons_paradox_data import *
 
 
-# In[3]:
+# In[2]:
 
 joint_prob_table[gender_mapping['female'], department_mapping['C'], admission_mapping['admitted']]
 
 
-# In[11]:
+# In[3]:
 
 joint_prob_table
 
 
-# In[13]:
+# In[4]:
 
 joint_prob_table.sum(axis=0)
 
 
-# In[10]:
+# In[5]:
 
 joint_prob_gender_admission = joint_prob_table.sum(axis=1)
 joint_prob_gender_admission
 
 
-# In[108]:
+# In[6]:
 
-pd.set_option('precision', 17)
-pd.set_eng_float_format(accuracy=5)
+import pandas as pd
+# pd.set_option('precision', 17)
+# pd.set_eng_float_format(accuracy=5)
 
 
-# In[112]:
+# In[7]:
 
 department_mapping
 
 
-# In[113]:
+# In[8]:
 
 joint_prob_panel = pd.Panel(joint_prob_table, 
          items = gender_labels,
@@ -48,153 +49,143 @@ joint_prob_panel = pd.Panel(joint_prob_table,
 joint_prob_panel
 
 
-# In[114]:
+# In[9]:
 
 joint_prob_panel.sum(axis=0)
 
 
-# In[22]:
+# In[10]:
 
 get_ipython().magic('matplotlib inline')
 
 
-# In[23]:
+# In[11]:
 
 joint_prob_panel.sum(axis=0).plot()
 
 
-# In[18]:
+# In[12]:
 
 joint_prob_panel.sum(axis=1)
 
 
-# In[24]:
+# In[13]:
 
 joint_prob_panel.sum(axis=1).plot()
 
 
-# In[25]:
+# In[14]:
 
 joint_prob_panel.sum(axis=2)
 
 
-# In[27]:
+# In[15]:
 
 joint_prob_panel.sum(axis=2).plot()
 
 
-# In[28]:
-
-joint_prob_gender_admission[gender_mapping['female'], admission_mapping['admitted']]
-
-
-# In[32]:
+# In[16]:
 
 joint_prob_panel.sum(axis=1)['female'].loc['admitted']
 
 
-# In[34]:
+# In[17]:
 
 female_only = joint_prob_gender_admission[gender_mapping['female']]
 female_only
 
 
-# In[35]:
+# In[18]:
 
 joint_prob_panel.sum(axis=1)['female']
 
 
-# In[37]:
+# In[19]:
 
 prob_admission_given_female = female_only / np.sum(female_only)
 prob_admission_given_female
 
 
-# In[40]:
+# In[20]:
 
 joint_prob_panel.sum(axis=1)['female']/joint_prob_panel.sum(axis=1)['female'].sum()
 
 
-# In[41]:
+# In[21]:
 
 prob_admission_given_female_dict = dict(zip(admission_labels, prob_admission_given_female))
 print(prob_admission_given_female_dict)
 
 
-# In[42]:
+# In[22]:
 
 joint_prob_panel.sum(axis=1)['male']/joint_prob_panel.sum(axis=1)['male'].sum()
 
 
-# In[44]:
+# In[23]:
 
 admitted_only = joint_prob_gender_admission[:, admission_mapping['admitted']]
 admitted_only
 
 
-# In[50]:
+# In[24]:
 
 joint_prob_panel.sum(axis=1).loc['admitted']
 
 
-# In[51]:
+# In[25]:
 
 prob_gender_given_admitted = admitted_only / np.sum(admitted_only)
 prob_gender_given_admitted_dict = dict(zip(gender_labels, prob_gender_given_admitted))
 print(prob_gender_given_admitted_dict)
 
 
-# In[53]:
+# In[26]:
 
 joint_prob_panel.sum(axis=1).loc['admitted']/joint_prob_panel.sum(axis=1).loc['admitted'].sum()
 
 
-# In[55]:
+# In[27]:
 
 female_and_A_only = joint_prob_table[gender_mapping['female'], department_mapping['A']]
 female_and_A_only
 
 
-# In[74]:
+# In[28]:
 
 joint_prob_table[gender_mapping['female'], department_mapping['B']]/joint_prob_table[gender_mapping['female'], department_mapping['B']].sum()
 
 
-# In[56]:
+# In[29]:
 
 joint_prob_panel
 
 
-# In[58]:
+# In[30]:
 
 joint_prob_panel['female'].loc['A']
 
 
-# In[96]:
+# In[31]:
 
 joint_prob_panel['male'].loc['D']['admitted']/joint_prob_panel['male'].loc['D'].sum()
 
 
-# In[72]:
+# In[32]:
 
 joint_prob_panel['female'].loc['B']/joint_prob_panel['female'].loc['B'].sum()
 
 
-# In[115]:
+# In[33]:
 
 joint_prob_panel['male']
 
 
-# In[117]:
+# In[34]:
 
 for d in department_labels:
     for g in gender_labels:
         print(g)
         print(joint_prob_panel[g].loc[d]/joint_prob_panel[g].loc[d].sum())
         print()
-
-
-# In[ ]:
-
-
 
